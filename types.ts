c1e423bb6661f375f2803b06ea72f29ec029cb45
theolecuyer <theoglecuyer@gmail.com>
2024-10-22 23:53:13 -0700

    Account types

--- types.ts
+++ types.ts
@@ -7,4 +7,11 @@ export type detailInfo = {
     date: string,
 }
 
+export type account = {
+    id: number;
+    image: string | null;
+    permissions: Permissions;
+}
+
+type Permissions = 'Owner' | 'Manager' | 'Employee'
 type Service = 'Interior' | 'Exterior' | 'Ceramic Coating' | 'Tint';
