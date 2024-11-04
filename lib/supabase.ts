e8954a1472310c9b6dad159ab2286603fe2acc32
theolecuyer <theoglecuyer@gmail.com>
2024-11-04 12:18:12 -0800

    Initial Supabase setup

---
+++ lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://zanphjupvpiyweglanme.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphbnBoanVwdnBpeXdlZ2xhbm1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3NTA3ODYsImV4cCI6MjA0NjMyNjc4Nn0.0K3sPSKrMrcX2z8dYgAEYxvCYxgAuBudaugMg-TMEgw';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
